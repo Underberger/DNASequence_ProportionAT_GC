72538708b2097b89ac05ab495db319cf57588b82
Lukas <lukas.schwella.sg@outlook.com>
2019-08-22 15:47:23 +0200

    Added the python script to the GitHub Repository

---
+++ DNASequence_ProportionAT_GC.py
@@ -0,0 +1,25 @@
+# User input DNA sequence
+try:
+    DNA_sequence = input("Please enter the DNA sequence below: \n")
+except:
+    print("Invalid, please enter a valid DNA sequence.")
+# DNA sequence to uppercases
+DNA = DNA_sequence.upper()
+# check for the nucleotides AT and GC and calculate the proportion
+count_AT = 0
+count_GC = 0
+for nuc in DNA:
+    if nuc == "A" or nuc == "T": # check for A or T nucleotide
+        count_AT += 1
+    else:   # else for G or C nucleotide
+        count_GC += 1
+
+total_nuc = count_AT + count_GC # calculate the total value of nucleotides
+proportion_AT = count_AT / total_nuc # proportion AT nucleotides
+proportion_GC = count_GC / total_nuc # proportion GC nucloetides
+
+pro_AT = float("{0:.2f}".format(proportion_AT)) # proportion rounded to two decimal points
+pro_GC = float("{0:.2f}".format(proportion_GC))
+
+print("AT bases " + str(count_AT) + ", GC bases " + str(count_GC) + 
+        "; Proportion AT " + str(proportion_AT) + ", proportion GC " + str(proportion_GC)) # print out values
